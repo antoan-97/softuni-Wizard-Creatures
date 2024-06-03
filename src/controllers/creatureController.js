36e1a98cb473d17f458c0b022eeb537a00ec783e
antoan-97 <antoan.todorov@abv.bg>
2024-06-03 12:35:26 +0300

    add 404 page

--- src/controllers/creatureController.js
+++ src/controllers/creatureController.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const creatureManager = require('../managers/creatureManager');
 
- router.get('/create', (req,res) =>{
+const { getErrorMessage } = require('../utils/errorHelper');
+
+router.get('/create', (req, res) => {
     res.render('create');
- });
+});
+
+router.post('/create', async (req, res) => {
+    const creatureData = {
+        ...req.body,
+        owner: req.user._id,
+    }
+
+    try {
+        await creatureManager.create(creatureData);
+        res.redirect('/')
+    } catch (err) {
+        res.render('creatures/create', { error: getErrorMessage(err) })
+    }
+});
 
 module.exports = router;
