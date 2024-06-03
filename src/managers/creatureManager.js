9ec6b50574e85bbb6017cf25ef65fd66a2c1d3e6
antoan-97 <antoan.todorov@abv.bg>
2024-06-03 14:43:25 +0300

    create getOne manager

--- src/managers/creatureManager.js
+++ src/managers/creatureManager.js
@@ -4,3 +4,5 @@ exports.create = (creatureData) => Creature.create(creatureData);
 
 exports.getAll = () => Creature.find().populate('owner');
 
+exports.getOne = (creatureId) => Creature.findById(creatureId).populate('owner');
+
