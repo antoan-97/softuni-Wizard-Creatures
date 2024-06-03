df0467cc5b3bdef5ed82cdd5c77318a7c6a69047
antoan-97 <antoan.todorov@abv.bg>
2024-06-03 12:20:15 +0300

    render create page

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const userController = require('./controllers/userController');
+const creatureController = require('./controllers/creatureController');
 
 
 
 router.use(homeController);
 router.use('/users',userController);
+router.use('/creatures',creatureController);
 
 
 module.exports = router
