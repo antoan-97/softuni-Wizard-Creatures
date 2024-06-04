f4e715957ed1ec4ba0f701751f87680a783a73bf
antoan-97 <antoan.todorov@abv.bg>
2024-06-04 16:04:03 +0300

    add vote manager function

--- src/managers/creatureManager.js
+++ src/managers/creatureManager.js
@@ -6,3 +6,19 @@ exports.getAll = () => Creature.find().populate('owner');
 
 exports.getOne = (creatureId) => Creature.findById(creatureId).populate('owner');
 
+exports.vote = async (creatureId, userId) => {
+    const creature = await Creature.findById(creatureId);
+
+    if (!creature) {
+        throw new Error('Creature not found');
+    }
+
+    // Check if the user has already voted
+    if (creature.votes.includes(userId)) {
+        return;
+    }
+
+    creature.votes.push(userId);
+    return creature.save();
+};
+
