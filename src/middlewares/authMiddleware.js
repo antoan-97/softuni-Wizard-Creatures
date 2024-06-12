6b95bc8debb50704b1ff6022369072905a6259f8
antoan-97 <antoan.todorov@abv.bg>
2024-06-12 14:48:18 +0300

    create middlewares

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require('../lib/jwt');
 const { SECRET } = require('../config/config');
+const creatureManager = require('../managers/creatureManager');
 
 
 exports.auth = async (req,res,next) => {
@@ -25,6 +26,24 @@ exports.auth = async (req,res,next) => {
 exports.isAuth = (req,res,next) =>{
     if(!req.user){
         res.redirect('/users/login');
+    }
+    next();
+};
+
+exports.isLoggedIn = (req, res, next) => {
+    if (req.user) {
+        // Redirect to 404 page  if user is already logged in
+        return res.render('404');
+    }
+    next();
+};
+
+
+exports.isOwner = async (req, res, next) => {
+    const creatureId = req.params.creatureId;
+    const creature = await creatureManager.getOne(creatureId);
+    if (!creature || creature.owner.toString() !== req.user._id.toString()) {
+        return res.render('404');
     }
     next();
 };
